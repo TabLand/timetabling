fe0e2fe53125252a1ea240e09805001c3990736a
Ijtaba Hussain <ijtabahussain@live.com>
2014-02-28 18:23:21 +0000

    Stub unit tests for bookings

---
+++ tests/booking_tests.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use Test::More tests=>5;
+use lib "..";
+use Booking;
+
+subtest 'cause room clash'{}
+
+subtest 'cause staff clash'{}
+
+subtest 'cause student clash'{}
+
+subtest 'no clash'{}
+
+subtest 'object creation'{}
