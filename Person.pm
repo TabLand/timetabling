bbc24c0c46c895d00ac354a4879b7e33d014e8bb
Ijtaba Hussain <ijtabahussain@live.com>
2014-03-05 14:30:50 +0000

    Working Person class (for staff and students) and passing unit tests

---
+++ Person.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+package Person;
+use strict;
+use warnings;
+
+sub new{
+	my ($class, $username, $name) = @_;
+	my $self = {
+		_username => $username,
+		_name => $name
+	};
+	bless $self, $class;
+	return $self;
+}
+sub get_name{
+	my $self = shift;
+	return $self->{_name};
+}
+sub get_username{
+	my $self = shift;
+	return $self->{_username};
+}
+sub equals{
+	my ($first, $second) = @_;
+	my $same_name = $first->get_name() eq $second->get_name();
+	my $same_username = $first->get_username() eq $second->get_username();
+	return $same_name && $same_username;
+}
+1;
---
+++ tests/Person.t
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use Test::More qw(no_plan);
+use lib "..";
+use Person;
+use strict;
+use warnings;
+
+subtest "Create" => sub{
+	my $person = new Person("abnd198", "Ijtaba Hussain");
+	isa_ok($person, "Person");
+	is($person->get_username(), "abnd198", "username getter works");
+	is($person->get_name(), "Ijtaba Hussain", "name getter works");
+};
+subtest "Equals" => sub{
+	my $person = new Person("abnd198", "Ijtaba Hussain");
+	my $same_person = new Person("abnd198", "Ijtaba Hussain");
+	ok($person->equals($same_person));
+};
