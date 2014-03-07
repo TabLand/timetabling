5f17d47d8e8955fbffb96dab613982af13985992
Ijtaba Hussain <ijtabahussain@live.com>
2014-03-07 17:44:43 +0000

    Interface package for use by each and every kind of constraint.
    Should consider adding SAT/Graph colouring functions to these? How??

---
+++ Constraint.pm
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+package Constraint;
+use strict;
+use warnings;
+
+sub new{
+	die "Supposed to be called from child class";
+}
+sub met{
+	die "Supposed to be called from child class";
+}
+sub get_clashes{
+	die "Supposed to be called from child class";
+}
+sub get_clash_info{
+	die "Supposed to be called from child class";
+}
+1;
