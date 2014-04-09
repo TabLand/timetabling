67e9fbeb9f444f90178b17c4dbf3faa6ad212560
Ijtaba Hussain <ijtabahussain@live.com>
2014-04-10 00:12:26 +0100

    checking character &npsp; \A0 and treating it as whitespace

--- ModuleGrabsToXML.pl
+++ ModuleGrabsToXML.pl
@@ -13,7 +13,7 @@ for my $grab (@grabs){
 	my $dom = Mojo::DOM->new($text);
 	my @collection = $dom->find("p, table.spreadsheet")->each;
 	my $day = "Noday";
-	my $xml_out = "";
+	my $xml_out = "<module>\n";
 	for my $element (@collection){
 		if($element->type eq "p"){
 			$day = get_day($element);
@@ -26,42 +26,60 @@ for my $grab (@grabs){
 			}
 		}
 	}
+	$xml_out .= "</module>";
 	save_xml("timetable_xml/$module.xml",$xml_out);
 }
 
 sub tr_to_xml{
 	my ($day, $table_datas) = @_;
-	my @tds = @$table_datas;
-	my $start_time = $tds[0]->text;
-	my $finish_time = $tds[1]->text;
-	my $week = $tds[2]->text;
-	my $module_code = $tds[3]->text;
-	my $activity = $tds[4]->text;
-	my $description = $tds[5]->text;
-	my $joint = $tds[6]->text;
-	my $student_numbers = $tds[7]->text;
-	my $room = $tds[8]->text;
-	my $lecturer = $tds[10]->text;
-	my $xml = "<booking>\n\t<start>$start_time</start>\n";
-	$xml .= "\t<end>$finish_time</end>\n";
-	$xml .= "\t<week>$week</week>\n";
-	$xml .= "\t<modulecode>$module_code</modulecode>\n";
-	$xml .= "\t<activity>$activity</activity>\n";
-	$xml .= "\t<desc>$description</desc>\n";
-	$xml .= "\t<joint>$joint</joint>\n";
-	$xml .= "\t<studentnos>$student_numbers</studentnos>\n";
-	$xml .= "\t<room>$room</room>\n";
-	$xml .= "\t<lecturer>$lecturer </lecturer>\n";
-	$xml .= "\t<day>$day </day>\n</booking>\n";
+	my @tds = test_white_space($table_datas);
+	my $start_time = $tds[0];
+	my $finish_time = $tds[1];
+	my $week = $tds[2];
+	my $module_code = $tds[3];
+	my $activity = $tds[4];
+	my $description = $tds[5];
+	my $joint = $tds[6];
+	my $student_numbers = $tds[7];
+	my $room = $tds[8];
+	my $lecturer = $tds[10];
+	my $xml = "\t<booking>\n\t\t<start>$start_time</start>\n";
+	$xml .= "\t\t<end>$finish_time</end>\n";
+	$xml .= "\t\t<week>$week</week>\n";
+	$xml .= "\t\t<modulecode>$module_code</modulecode>\n";
+	$xml .= "\t\t<activity>$activity</activity>\n";
+	$xml .= "\t\t<desc>$description</desc>\n";
+	$xml .= "\t\t<joint>$joint</joint>\n";
+	$xml .= "\t\t<studentnos>$student_numbers</studentnos>\n";
+	$xml .= "\t\t<room>$room</room>\n";
+	$xml .= "\t\t<lecturer>$lecturer </lecturer>\n";
+	$xml .= "\t\t<day>$day </day>\n\t</booking>\n";
 }
-
+sub test_white_space{
+	my $tds_ref = shift;
+	my @tds = @$tds_ref;
+	my @out;
+	for my $td (@tds){
+		
+		if($td->text=~/^\s*$/){
+			push(@out, "-");
+		}
+		elsif($td->text=~/\xa0/){
+			push(@out, "-");
+		}
+		else {
+			push(@out, $td->text);
+		}
+	}
+	return @out;
+}
 sub get_day{
 	my $element = shift;
 	return $element->find(".labelone")->text;
 }
 sub get_file_contents{
 	my $filepath = get_full_path(shift);
-	open(my $file, "<", $filepath) or die "Unable to open file for reading, $!";;
+	open(my $file, "<", $filepath) or die "Unable to open file $filepath for reading, $!";;
 	my $text = join('', <$file>);
 	close($file);
 	return $text;
